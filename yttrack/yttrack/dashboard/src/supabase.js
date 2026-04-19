ff36128c2871d56acd1e6f37ee67a899814afa22
Mysticslayerdd16 <dipeshdeshmukhdd16@gmail.com>
2026-04-19 18:17:16 +0530

    Delete yttrack/yttrack directory

--- yttrack/yttrack/dashboard/src/supabase.js
+++
@@ -1,6 +0,0 @@
-import { createClient } from '@supabase/supabase-js';
-
-const SUPABASE_URL = 'https://uxajpjxypltpqnleapis.supabase.co';
-const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InV4YWpwanh5cGx0cHFubGVhcGlzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzY2MDAyNzgsImV4cCI6MjA5MjE3NjI3OH0.6xNLPxcuzW42BZ3fskQHNSB7HYFqHS7tg1EJFMvrEXs';
-
-export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
